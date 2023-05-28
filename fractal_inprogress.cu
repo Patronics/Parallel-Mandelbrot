f2da4d8eec42bf127562ffd126e23c78c7133b68
j-p-ecs <jpugh@ucdavis.edu>
2023-05-27 21:15:10 -0700

    working on cuda implementation

---
+++ fractal_inprogress.cu
@@ -0,0 +1,328 @@
+/*
+<<<<<<< Updated upstream
+fractal.cu - Parallel interactive Mandelbrot Fractal Display
+=======
+fractal.c - Parallel interactive Mandelbrot Fractal Display
+>>>>>>> Stashed changes
+based on starting code for CSE 30341 Project 3.
+*/
+
+#include "gfx.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <errno.h>
+#include <string.h>
+#include <complex.h>
+#include <time.h>
+
+#include <cuda.h>
+
+<<<<<<< Updated upstream
+=======
+#define WARPSZ 32 
+#define MAX_BLKSZ 1024
+
+>>>>>>> Stashed changes
+typedef struct coordSet {
+	double xmin;
+	double xmax;
+	double ymin;
+	double ymax;
+	int maxiter;
+	double xmid;
+	double ymid;
+} coordSet;
+
+
+/*
+Compute the number of iterations at point x, y
+in the complex space, up to a maximum of maxiter.
+Return the number of iterations at that point.
+
+This example computes the Mandelbrot fractal:
+z = z^2 + alpha
+
+Where z is initially zero, and alpha is the location x + iy
+in the complex plane.  Note that we are using the "complex"
+numeric type in C, which has the special functions cabs()
+and cpow() to compute the absolute values and powers of
+complex values.
+*/
+
+<<<<<<< Updated upstream
+static int compute_point( double x, double y, int max )
+{
+	double complex z = 0;
+	double complex alpha = x + I*y;
+
+	int iter = 0;
+
+	if (y == 0) {
+		while (cabs(z) < 4 && iter < max) {
+			z = cpow(z, 2) + alpha;
+			iter++;
+		}
+	}
+	else {
+		while (cabs(z) < 4 && iter < max) {
+			z = z * z + alpha;
+			iter++;
+		}
+	}
+
+=======
+__device__ int compute_point( double x, double y, int max )
+{
+	double z_real = 0;
+                double z_imaginary = 0;
+                double z_realsquared = 0;
+                double z_imaginarysquared = 0;
+
+	int iter = 0;
+	for(iter = 0; iter < max; iter++) {
+	z_imaginary = z_real * z_imaginary;
+                                z_imaginary = z_imaginary + z_imaginary + y;
+                                z_real = z_realsquared - z_imaginarysquared + x;
+                                z_realsquared = z_real * z_real;
+                                z_imaginarysquared = z_imaginary * z_imaginary;
+                                if (z_realsquared + z_imaginarysquared >= 4.0) {
+                                        iter++;
+                                        break;
+                                }
+	}
+>>>>>>> Stashed changes
+	return iter;
+}
+
+/*
+Compute an entire image, writing each point to the given bitmap.
+Scale the image to the range (xmin-xmax,ymin-ymax).
+*/
+
+__global__ void compute_image(coordSet* coords)
+{
+	double xmin=coords->xmin;
+	double xmax=coords->xmax;
+	double ymin=coords->ymin;
+	double ymax=coords->ymax;
+	int maxiter=coords->maxiter;
+
+    int my_i = blockDim.x * blockIdx.x + threadIdx.x;
+    int my_j = blockDim.y * blockIdx.y + threadIdx.y;
+
+	int width = gfx_xsize();
+	int height = gfx_ysize();
+
+    double x = xmin + my_i*(xmax-xmin)/width;
+	double y = ymin + my_j*(ymax-ymin)/height;
+
+    int iter = 0;
+    iter = compute_point(x,y,maxiter);
+
+    int r = 255 * iter / maxiter;
+	int g = 255 * iter / (maxiter/30);
+	int b = 255 * iter / (maxiter/100);
+
+    {
+		gfx_color(r,g,b);
+		// Plot the point on the screen.
+		gfx_point(my_i,my_j);
+	}
+}
+
+void setMidpoints(coordSet* coords){
+	coords->xmid = (coords->xmin+coords->xmax)/2;
+	coords->ymid = (coords->ymin+coords->ymax)/2;
+
+}
+
+void reDraw(coordSet* coords){
+    int width = gfx_xsize();
+	int height = gfx_ysize();
+
+    int thread_count = width * height;
+	// Show the configuration, just in case you want to recreate it.
+	printf("coordinates: %lf %lf %lf %lf\n",coords->xmin,coords->xmax,coords->ymin,coords->ymax);
+	// Display the fractal image
+
+	struct timespec startTime, endTime;
+	double runTime;
+	clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+	// this is not the actual block size and thread count
+	compute_image <<<1, thread_count>>>(coords);
+    cudaDeviceSynchronize();
+
+	clock_gettime(CLOCK_MONOTONIC, &endTime);
+	runTime = difftime(endTime.tv_sec, startTime.tv_sec)+((endTime.tv_nsec-startTime.tv_nsec)/1e9);
+	fprintf(stderr, "\nrendering frame took %lf seconds\n", runTime);
+
+}
+
+
+void zoomIn(coordSet* coords,double extent){
+	setMidpoints(coords);
+	double width = coords->xmid-coords->xmin;
+	double height = coords->ymid-coords->ymin;
+	coords->xmax=coords->xmid+(width/extent);
+	coords->xmin=coords->xmid-(width/extent);
+	coords->ymax=coords->ymid+(height/extent);
+	coords->ymin=coords->ymid-(height/extent);
+	setMidpoints(coords);
+	reDraw(coords);
+}
+
+void zoomOut(coordSet* coords, double extent){
+	setMidpoints(coords);
+	double width = coords->xmid-coords->xmin;
+	double height = coords->ymid-coords->ymin;
+	coords->xmax=coords->xmid+(width*extent);
+	coords->xmin=coords->xmid-(width*extent);
+	coords->ymax=coords->ymid+(height*extent);
+	coords->ymin=coords->ymid-(height*extent);
+	setMidpoints(coords);
+	reDraw(coords);
+}
+
+void shiftFrame(coordSet* coords, double xShift, double yShift){
+	setMidpoints(coords);
+	double width = coords->xmax-coords->xmin;
+	double height = coords->ymax-coords->ymin;
+	coords->xmax+=xShift*width;
+	coords->xmin+=xShift*width;
+	coords->ymax+=yShift*height;
+	coords->ymin+=yShift*height;
+	setMidpoints(coords);
+	reDraw(coords);
+}
+
+
+//accidentally discovered, mirrors coords, may be useful
+void reflect(coordSet* coords){
+	setMidpoints(coords);
+	coords->xmax=coords->xmax+(coords->xmid-coords->xmax)*2;
+	coords->xmin=coords->xmin+(coords->xmid-coords->xmin)*2;
+	coords->ymax=coords->ymax+(coords->ymid-coords->ymax)*2;
+	coords->ymin=coords->ymin+(coords->ymid-coords->ymin)*2;
+	setMidpoints(coords);
+	reDraw(coords);
+}
+
+
+int main( int argc, char *argv[] ){
+	// The initial boundaries of the fractal image in x,y space.
+	const double xminDefault = -1.5;
+	const double xmaxDefault = 0.5;
+	const double yminDefault = -1.0;
+	const double ymaxDefault=  1.0;
+	// Maximum number of iterations to compute.
+	// Higher values take longer but have more detail.
+	const int maxiterDefault = 3000; //default 500
+	
+<<<<<<< Updated upstream
+	coordSet* dispCoords = malloc(sizeof(coordSet));
+=======
+	coordSet* dispCoords = (coordSet*) malloc(sizeof(coordSet));
+>>>>>>> Stashed changes
+	
+	if(argv[1] && argv[2] && argv[3] && argv[4] && argv[5]){
+		dispCoords->xmin = atof(argv[1]);
+		dispCoords->xmax = atof(argv[2]);
+		dispCoords->ymin = atof(argv[3]);
+		dispCoords->ymax = atof(argv[4]);
+		dispCoords->maxiter = atoi(argv[5]);
+		setMidpoints(dispCoords);
+	}else{
+		dispCoords->xmin=xminDefault;
+		dispCoords->xmax=xmaxDefault;
+		dispCoords->ymin=yminDefault;
+		dispCoords->ymax=ymaxDefault;
+		dispCoords->maxiter=maxiterDefault;
+		setMidpoints(dispCoords);
+	}
+
+
+	// Open a new window.
+	gfx_open(640,480,"Mandelbrot Fractal");
+
+
+	// Fill it with a dark blue initially.
+	gfx_clear_color(0,0,255);
+	gfx_clear();
+
+	//draw intial position
+	reDraw(dispCoords);
+
+
+	while(1) {
+		// Wait for a key or mouse click.
+		int c = gfx_wait();
+		printf("got character %c\n",c);
+		// Quit if q is pressed.
+		switch(c){
+		case 'q':
+			free(dispCoords);
+			exit(0);
+		//reset default position
+		case 'r':
+			dispCoords->xmin=xminDefault;
+			dispCoords->xmax=xmaxDefault;
+			dispCoords->ymin=yminDefault;
+			dispCoords->ymax=ymaxDefault;
+			setMidpoints(dispCoords);
+			reDraw(dispCoords);
+			break;
+		//Reflect the view (mirroring it)
+		case 'R':
+			reflect(dispCoords);
+			break;
+		//zoom in/out with i/o (or smoothly with I/O)
+		case 'i':
+			printf("zooming in\n");
+			zoomIn(dispCoords, 2);
+			break;
+		case 'o':
+			printf("zooming out\n");
+			zoomOut(dispCoords, 2);
+			break;
+		case 'I':
+			printf("zooming in slightly\n");
+			zoomIn(dispCoords, 1.25);
+			break;
+		case 'O':
+			printf("zooming out slightly\n");
+			zoomOut(dispCoords, 1.25);
+			break;
+		//pan with wasd (or smoothly with WASD)
+		case 'w':
+			shiftFrame(dispCoords, 0, -0.5);
+			break;
+		case 'W':
+			shiftFrame(dispCoords, 0, -0.25);
+			break;
+		case 's':
+			shiftFrame(dispCoords, 0, 0.5);
+			break;
+		case 'S':
+			shiftFrame(dispCoords, 0, 0.25);
+			break;
+		case 'a':
+			shiftFrame(dispCoords, -0.5, 0);
+			break;
+		case 'A':
+			shiftFrame(dispCoords, -0.25, 0);
+			break;
+		case 'd':
+			shiftFrame(dispCoords, 0.5, 0);
+			break;
+		case 'D':
+			shiftFrame(dispCoords, 0.25, 0);
+			break;
+		}
+//		} else if(c=='q'){
+	}
+
+	return 0;
+}
