1b5f6d313f3cecf334d94a517aa9dd14cd731461
Patrick Leiser <pleiser@ucdavis.edu>
2023-06-02 15:43:19 -0700

    complete merge to main

--- fractal.c
+++ fractal.c
@@ -41,10 +41,10 @@ and cpow() to compute the absolute values and powers of
 complex values.
 */
 
-static int compute_point(double x, double y, int max)
+static int compute_point( double x, double y, int max )
 {
 	double complex z = 0;
-	double complex alpha = x + I * y;
+	double complex alpha = x + I*y;
 
 	int iter = 0;
 
@@ -70,55 +70,55 @@ Scale the image to the range (xmin-xmax,ymin-ymax).
 
 void compute_image(coordSet* coords)
 {
-	int i, j;
-	double xmin = coords->xmin;
-	double xmax = coords->xmax;
-	double ymin = coords->ymin;
-	double ymax = coords->ymax;
-	int maxiter = coords->maxiter;
+	int i,j;
+	double xmin=coords->xmin;
+	double xmax=coords->xmax;
+	double ymin=coords->ymin;
+	double ymax=coords->ymax;
+	int maxiter=coords->maxiter;
 
 	int width = gfx_xsize();
 	int height = gfx_ysize();
 
 	// For every pixel i,j, in the image...
-#pragma omp parallel for schedule(dynamic)
-	for (j = 0; j < height; j++) {
-#pragma omp parallel for schedule(dynamic)
-		for (i = 0; i < width; i++) {
+	#pragma omp parallel for schedule(dynamic)
+	for(j=0;j<height;j++) {
+		#pragma omp parallel for schedule(dynamic)
+		for(i=0;i<width;i++) {
 			// Scale from pixels i,j to coordinates x,y
-			double x = xmin + i * (xmax - xmin) / width;
-			double y = ymin + j * (ymax - ymin) / height;
+			double x = xmin + i*(xmax-xmin)/width;
+			double y = ymin + j*(ymax-ymin)/height;
 
 			// Compute the iterations at x,y
 			int iter = 0;
 			//#pragma omp critical
-			iter = compute_point(x, y, maxiter);
+			iter = compute_point(x,y,maxiter);
 
 			// Convert a iteration number to an RGB color.
 			// (Change this bit to get more interesting colors.)
 			//int gray = 255 * iter / maxiter;
 			int r = 255 * iter / maxiter;
-			int g = 255 * iter / (maxiter / 30);
-			int b = 255 * iter / (maxiter / 100);
-#pragma omp critical (plotpixel)
+			int g = 255 * iter / (maxiter/30);
+			int b = 255 * iter / (maxiter/100);
+			#pragma omp critical (plotpixel)
 			{
-				gfx_color(r, g, b);
+				gfx_color(r,g,b);
 				// Plot the point on the screen.
-				gfx_point(i, j);
+				gfx_point(i,j);
 			}
 		}
 	}
 }
 
-void setMidpoints(coordSet* coords) {
-	coords->xmid = (coords->xmin + coords->xmax) / 2;
-	coords->ymid = (coords->ymin + coords->ymax) / 2;
+void setMidpoints(coordSet* coords){
+	coords->xmid = (coords->xmin+coords->xmax)/2;
+	coords->ymid = (coords->ymin+coords->ymax)/2;
 
 }
 
-void reDraw(coordSet* coords) {
+void reDraw(coordSet* coords){
 	// Show the configuration, just in case you want to recreate it.
-	printf("coordinates: %lf %lf %lf %lf\n", coords->xmin, coords->xmax, coords->ymin, coords->ymax);
+	printf("coordinates: %lf %lf %lf %lf\n",coords->xmin,coords->xmax,coords->ymin,coords->ymax);
 	// Display the fractal image
 
 	struct timespec startTime, endTime;
@@ -128,126 +128,125 @@ void reDraw(coordSet* coords) {
 	compute_image(coords);
 
 	clock_gettime(CLOCK_MONOTONIC, &endTime);
-	runTime = difftime(endTime.tv_sec, startTime.tv_sec) + ((endTime.tv_nsec - startTime.tv_nsec) / 1e9);
+	runTime = difftime(endTime.tv_sec, startTime.tv_sec)+((endTime.tv_nsec-startTime.tv_nsec)/1e9);
 	fprintf(stderr, "\nrendering frame took %lf seconds\n", runTime);
 
 }
 
 
-void zoomIn(coordSet* coords, double extent) {
+void zoomIn(coordSet* coords,double extent){
 	setMidpoints(coords);
-	double width = coords->xmid - coords->xmin;
-	double height = coords->ymid - coords->ymin;
-	coords->xmax = coords->xmid + (width / extent);
-	coords->xmin = coords->xmid - (width / extent);
-	coords->ymax = coords->ymid + (height / extent);
-	coords->ymin = coords->ymid - (height / extent);
-	setMidpoints(coords);
-	reDraw(coords);
-}
-
-void zoomOut(coordSet* coords, double extent) {
-	setMidpoints(coords);
-	double width = coords->xmid - coords->xmin;
-	double height = coords->ymid - coords->ymin;
-	coords->xmax = coords->xmid + (width * extent);
-	coords->xmin = coords->xmid - (width * extent);
-	coords->ymax = coords->ymid + (height * extent);
-	coords->ymin = coords->ymid - (height * extent);
+	double width = coords->xmid-coords->xmin;
+	double height = coords->ymid-coords->ymin;
+	coords->xmax=coords->xmid+(width/extent);
+	coords->xmin=coords->xmid-(width/extent);
+	coords->ymax=coords->ymid+(height/extent);
+	coords->ymin=coords->ymid-(height/extent);
 	setMidpoints(coords);
 	reDraw(coords);
 }
 
-void shiftFrame(coordSet* coords, double xShift, double yShift) {
+void zoomOut(coordSet* coords, double extent){
 	setMidpoints(coords);
-	double width = coords->xmax - coords->xmin;
-	double height = coords->ymax - coords->ymin;
-	coords->xmax += xShift * width;
-	coords->xmin += xShift * width;
-	coords->ymax += yShift * height;
-	coords->ymin += yShift * height;
+	double width = coords->xmid-coords->xmin;
+	double height = coords->ymid-coords->ymin;
+	coords->xmax=coords->xmid+(width*extent);
+	coords->xmin=coords->xmid-(width*extent);
+	coords->ymax=coords->ymid+(height*extent);
+	coords->ymin=coords->ymid-(height*extent);
+	setMidpoints(coords);
+	reDraw(coords);
+}
+
+void shiftFrame(coordSet* coords, double xShift, double yShift){
+	setMidpoints(coords);
+	double width = coords->xmax-coords->xmin;
+	double height = coords->ymax-coords->ymin;
+	coords->xmax+=xShift*width;
+	coords->xmin+=xShift*width;
+	coords->ymax+=yShift*height;
+	coords->ymin+=yShift*height;
 	setMidpoints(coords);
 	reDraw(coords);
 }
 
 
 //accidentally discovered, mirrors coords, may be useful
-void reflect(coordSet* coords) {
+void reflect(coordSet* coords){
 	setMidpoints(coords);
-	coords->xmax = coords->xmax + (coords->xmid - coords->xmax) * 2;
-	coords->xmin = coords->xmin + (coords->xmid - coords->xmin) * 2;
-	coords->ymax = coords->ymax + (coords->ymid - coords->ymax) * 2;
-	coords->ymin = coords->ymin + (coords->ymid - coords->ymin) * 2;
+	coords->xmax=coords->xmax+(coords->xmid-coords->xmax)*2;
+	coords->xmin=coords->xmin+(coords->xmid-coords->xmin)*2;
+	coords->ymax=coords->ymax+(coords->ymid-coords->ymax)*2;
+	coords->ymin=coords->ymin+(coords->ymid-coords->ymin)*2;
 	setMidpoints(coords);
 	reDraw(coords);
 }
 
 
-int main(int argc, char* argv[]) {
+int main( int argc, char *argv[] ){
 	// The initial boundaries of the fractal image in x,y space.
 	const double xminDefault = -1.5;
 	const double xmaxDefault = 0.5;
 	const double yminDefault = -1.0;
-	const double ymaxDefault = 1.0;
+	const double ymaxDefault=  1.0;
 	// Maximum number of iterations to compute.
 	// Higher values take longer but have more detail.
 	const int maxiterDefault = 3000; //default 500
-
+	
 	coordSet* dispCoords = malloc(sizeof(coordSet));
-
-	if (argv[1] && argv[2] && argv[3] && argv[4] && argv[5]) {
+	
+	if(argv[1] && argv[2] && argv[3] && argv[4] && argv[5]){
 		dispCoords->xmin = atof(argv[1]);
 		dispCoords->xmax = atof(argv[2]);
 		dispCoords->ymin = atof(argv[3]);
 		dispCoords->ymax = atof(argv[4]);
 		dispCoords->maxiter = atoi(argv[5]);
 		setMidpoints(dispCoords);
-	}
-	else {
-		dispCoords->xmin = xminDefault;
-		dispCoords->xmax = xmaxDefault;
-		dispCoords->ymin = yminDefault;
-		dispCoords->ymax = ymaxDefault;
-		dispCoords->maxiter = maxiterDefault;
+	}else{
+		dispCoords->xmin=xminDefault;
+		dispCoords->xmax=xmaxDefault;
+		dispCoords->ymin=yminDefault;
+		dispCoords->ymax=ymaxDefault;
+		dispCoords->maxiter=maxiterDefault;
 		setMidpoints(dispCoords);
 	}
 
 
 	// Open a new window.
-	gfx_open(640, 480, "Mandelbrot Fractal");
+	gfx_open(640,480,"Mandelbrot Fractal");
 
 
 	// Fill it with a dark blue initially.
-	gfx_clear_color(0, 0, 255);
+	gfx_clear_color(0,0,255);
 	gfx_clear();
 
 	//draw intial position
 	reDraw(dispCoords);
 
 
-	while (1) {
+	while(1) {
 		// Wait for a key or mouse click.
 		int c = gfx_wait();
-		printf("got character %c\n", c);
+		printf("got character %c\n",c);
 		// Quit if q is pressed.
-		switch (c) {
+		switch(c){
 		case 'q':
 			free(dispCoords);
 			exit(0);
-			//reset default position
+		//reset default position
 		case 'r':
-			dispCoords->xmin = xminDefault;
-			dispCoords->xmax = xmaxDefault;
-			dispCoords->ymin = yminDefault;
-			dispCoords->ymax = ymaxDefault;
+			dispCoords->xmin=xminDefault;
+			dispCoords->xmax=xmaxDefault;
+			dispCoords->ymin=yminDefault;
+			dispCoords->ymax=ymaxDefault;
 			setMidpoints(dispCoords);
 			reDraw(dispCoords);
 			break;
-			//Reflect the view (mirroring it)
+		//Reflect the view (mirroring it)
 		case 'R':
 			reflect(dispCoords);
 			break;
-			//zoom in/out with i/o (or smoothly with I/O)
+		//zoom in/out with i/o (or smoothly with I/O)
 		case 'i':
 			printf("zooming in\n");
 			zoomIn(dispCoords, 2);
@@ -264,7 +263,7 @@ int main(int argc, char* argv[]) {
 			printf("zooming out slightly\n");
 			zoomOut(dispCoords, 1.25);
 			break;
-			//pan with wasd (or smoothly with WASD)
+		//pan with wasd (or smoothly with WASD)
 		case 'w':
 			shiftFrame(dispCoords, 0, -0.5);
 			break;
@@ -290,7 +289,7 @@ int main(int argc, char* argv[]) {
 			shiftFrame(dispCoords, 0.25, 0);
 			break;
 		}
-		//		} else if(c=='q'){
+//		} else if(c=='q'){
 	}
 
 	return 0;
