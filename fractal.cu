8d383c15f32c971089303f66eec8fe1891e537e1
catheriny09 <catheriny09@gmail.com>
2023-06-14 02:00:21 -0700

    dimension test

--- fractal.cu
+++ fractal.cu
@@ -6,10 +6,10 @@ extern "C" {
 #include "gfx.h"
 }
 
-#define WIDTH 1280
-#define HEIGHT 960
-//#define WIDTH 640
-//#define HEIGHT 480
+//#define WIDTH 1280
+//#define HEIGHT 960
+#define WIDTH 640
+#define HEIGHT 480
 
 #include <stdlib.h>
 #include <stdio.h>
