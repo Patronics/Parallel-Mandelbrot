e0b31c8fa86c36f730bb6aaf220ca1f61632b2f2
j-p-ecs <jpugh@ucdavis.edu>
2023-06-14 20:47:06 -0700

    temp soln

--- fractal_cache.cu
+++ fractal_cache.cu
@@ -19,7 +19,6 @@ extern "C" {
 #include <complex.h>
 #include <time.h>
 #include <cuda.h>
-#include <omp.h>
 
 typedef struct coordSet {
 	double xmin;
@@ -31,7 +30,7 @@ typedef struct coordSet {
 	double ymid;
 	double xShift;
 	double yShift;
-	int zoom;
+	double  zoom;
 } coordSet;
 
 
@@ -41,18 +40,21 @@ struct colors {
 	uint8_t b;
 };
 
-struct cache {
-	struct colors hashmap[WIDTH * HEIGHT];
-};
-
-int blockSize;
-int blockCount;
 int windowWidth;
 int windowHeight;
+int blockSize;
+int blockCount;
+
+struct cache {
+	struct colors hashmap[4194304];
+};
+
 /*
 Compute the number of iterations at point x, y
 in the complex space, up to a maximum of maxiter.
 Return the number of iterations at that point.
+
+
 
 This example computes the Mandelbrot fractal:
 z = z^2 + alpha
@@ -101,14 +103,15 @@ __global__ void compute_image(coordSet* coords, int width, int height, struct co
 	double xShift=coords->xShift;
 	double yShift=coords->yShift;
 	int maxiter=coords->maxiter;
-	int zoom=coords->zoom;
-	int my_a = (blockDim.x * blockIdx.x + threadIdx.x) % width;
+	double zoom=coords->zoom;
+int my_a = (blockDim.x * blockIdx.x + threadIdx.x) % width;
 	int my_b = (blockDim.x * blockIdx.x + threadIdx.x) / width;
 	int stepx = (blockCount * blockSize) % width;
 	int stepy = (blockCount * blockSize) / width;
 	int carry;
     //int my_i = blockDim.x * blockIdx.x + threadIdx.x;
     //int my_j = blockDim.y * blockIdx.y + threadIdx.y;
+
 	for(int my_i = my_a, my_j = my_b; my_i < width && my_j < height*balance; my_i = (my_i + stepx) % width, my_j = my_j + stepy + carry) {
 		
 		carry = 0;
@@ -153,67 +156,7 @@ __global__ void compute_image(coordSet* coords, int width, int height, struct co
 			colorsSet[my_i+width*my_j].b = ch->hashmap[key].b;
 		}
 		if(my_i + stepx >= width) carry = 1;
-	
-		
 	}
-}
-uint16_t compute_pointCPU( double x, double y, uint16_t max )
-{
-        double z_real = 0;
-        double z_imaginary = 0;
-        double z_realsquared = 0;
-        double z_imaginarysquared = 0;
-
-        uint16_t iter = 0;
-	//if(x > -0.6 && x < 0.2 && y < -0.3 && y > 0.3) return max;
-
-       	for (iter = 0; iter < max; ++iter) {
-                z_imaginary = z_real * z_imaginary;
-                z_imaginary = z_imaginary + z_imaginary + y;
-                z_real = z_realsquared - z_imaginarysquared + x;
-                z_realsquared = z_real * z_real;
-                z_imaginarysquared = z_imaginary * z_imaginary;
-                if (z_realsquared + z_imaginarysquared >= 4.0) {
-                        ++iter;
-                        break;
-                }
-        }
-
-        return iter;
-}
-
-void compute_imageCPU(coordSet* coords, int width, int height, struct colors *colorsSet, double balance)
-{
-        double xmin=coords->xmin;
-        double xmax=coords->xmax;
-        double ymin=coords->ymin;
-        double ymax=coords->ymax;
-        int maxiter=coords->maxiter;
-	double start_time = omp_get_wtime();
-    //int my_i = blockDim.x * blockIdx.x + threadIdx.x;
-    //int my_j = blockDim.y * blockIdx.y + threadIdx.y;
-        //int total_threads = gridDim.x * blockDim.x;
-        //int total_threads1 = gridDim.y * blockDim.y;
-	#pragma omp parallel for schedule(dynamic)
-        for(int i = height*balance; i < height; ++i) {
-
-		#pragma omp parallel for schedule(dynamic)
-		for(int j = 0; j < width; ++j) {
-
-    double x = xmin + j*(xmax-xmin)/width;
-        double y = ymin + i*(ymax-ymin)/height;
-
-    uint16_t iter = compute_pointCPU(x,y,maxiter);
-    colorsSet[i*width+j].r = 255 * iter / maxiter;
-        colorsSet[i*width+j].g = 255 * iter / (maxiter/30);
-        colorsSet[i*width+j].b = 255 * iter / (maxiter/100);
-	//colorsSet[i*width+j].r = (colorsArray[iter] & 0xFF0000) >> 16;
-        //colorsSet[i*width+j].b = (colorsArray[iter] & 0xFF00) >> 8;
-        //colorsSet[i*width+j].g = colorsArray[iter] & 0xFF;
-        }
-	}
-	double end_time = omp_get_wtime();
-        printf("%.5f\n", end_time - start_time);
 }
 
 void draw_point(int i, int j, struct colors c)
@@ -231,18 +174,15 @@ void setMidpoints(coordSet* coords){
 void reDraw(coordSet* coords){
 	static struct cache* ch = (struct cache*)malloc(sizeof(struct cache));
 
-    int width = windowWidth;
-	int height = windowHeight;
+    int width = gfx_xsize();
+	int height = gfx_ysize();
 
     int n = width * height;
-
-    	double balance = 1.0;
-	if(n > 100000000) balance = 0.6;
 	
-	//#define BLOCK_SIZE 16 //TODO bigger blocks are likely faster
+	#define BLOCK_SIZE 16 //TODO bigger blocks are likely faster
 	
 	//dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE); // so your threads are BLOCK_SIZE*BLOCK_SIZE, 256 in this case
-	dim3 dimGrid(width*width/blockCount, height*height/blockCount); // 1*1 blocks in a grid
+	//dim3 dimGrid(width/BLOCK_SIZE, height/BLOCK_SIZE); // 1*1 blocks in a grid
 
 	struct colors* colorsSet;
 	coordSet* cudaCoords;
@@ -252,7 +192,7 @@ void reDraw(coordSet* coords){
 	cudaMalloc(&cudaCache, sizeof(struct cache));
 	cudaMalloc(&colorsSet, n * sizeof(struct colors));
 	cudaMalloc(&cudaCoords, sizeof(coordSet));
-
+	double balance = 1.0;
 	// Show the configuration, just in case you want to recreate it.
 	printf("coordinates: %lf %lf %lf %lf\n",coords->xmin,coords->xmax,coords->ymin,coords->ymax);
 	// Display the fractal image
@@ -267,39 +207,35 @@ void reDraw(coordSet* coords){
 	err = cudaMemcpy(colorsSet, c, n * sizeof(struct colors), cudaMemcpyHostToDevice);
 	if (err != cudaSuccess) printf("%s memcpy1\n", cudaGetErrorString(err));
 
-	//cudaMemcpy(cudaCache, ch, sizeof(struct cache), cudaMemcpyHostToDevice);
-	//if (err != cudaSuccess) printf("%s memcpy2\n", cudaGetErrorString(err));
-	if(balance > 0.0) {
+	cudaMemcpy(cudaCache, ch, sizeof(struct cache), cudaMemcpyHostToDevice);
+	if (err != cudaSuccess) printf("%s memcpy2\n", cudaGetErrorString(err));
+
 	compute_image <<<blockCount, blockSize>>>(cudaCoords, width, height, colorsSet, cudaCache, blockCount, blockSize, balance);
-	}
-	compute_imageCPU(coords, width, height, c, balance);
-	if(balance > 0.0) {
+
 	err = cudaDeviceSynchronize();
 	if (err != cudaSuccess) printf("%s synch\n", cudaGetErrorString(err));
 
-	err = cudaMemcpy(c, colorsSet, n * balance * sizeof(struct colors), cudaMemcpyDeviceToHost);
+	err = cudaMemcpy(c, colorsSet, n * sizeof(struct colors), cudaMemcpyDeviceToHost);
 	if (err != cudaSuccess) printf("%s memcpy3\n", cudaGetErrorString(err));
 
-	//err = cudaMemcpy(ch, cudaCache, sizeof(struct cache), cudaMemcpyDeviceToHost);
-	//if (err != cudaSuccess) printf("%s memcpy4\n", cudaGetErrorString(err));
+	err = cudaMemcpy(ch, cudaCache, sizeof(struct cache), cudaMemcpyDeviceToHost);
+	if (err != cudaSuccess) printf("%s memcpy4\n", cudaGetErrorString(err));
 
 	err = cudaDeviceSynchronize();
 	if (err != cudaSuccess) printf("%s synch2\n", cudaGetErrorString(err));
-	}
+	
 	clock_gettime(CLOCK_MONOTONIC, &endTime);
 	runTime = difftime(endTime.tv_sec, startTime.tv_sec)+((endTime.tv_nsec-startTime.tv_nsec)/1e9);
 	fprintf(stderr, "\ncalculating frame took %lf seconds\n", runTime);
 	
 	for (int i = 0; i < height; i++)
 		for (int j = 0; j < width; j++){
-			
 			draw_point(i, j, c[i * width + j]);
-			
 		}
 	clock_gettime(CLOCK_MONOTONIC, &endTime);
 	runTime = difftime(endTime.tv_sec, startTime.tv_sec)+((endTime.tv_nsec-startTime.tv_nsec)/1e9);
 	fprintf(stderr, "\ncalculating and rendering frame took %lf seconds\n", runTime);
-	
+
 	free(c);
 	cudaFree(colorsSet);
 	cudaFree(cudaCoords);
@@ -315,7 +251,7 @@ void zoomIn(coordSet* coords,double extent){
 	coords->xmin=coords->xmid-(width/extent);
 	coords->ymax=coords->ymid+(height/extent);
 	coords->ymin=coords->ymid-(height/extent);
-	coords->zoom -= 1;
+	coords->zoom -= extent;
 	setMidpoints(coords);
 	reDraw(coords);
 }
@@ -328,7 +264,7 @@ void zoomOut(coordSet* coords, double extent){
 	coords->xmin=coords->xmid-(width*extent);
 	coords->ymax=coords->ymid+(height*extent);
 	coords->ymin=coords->ymid-(height*extent);
-	coords->zoom += 1;
+	coords->zoom += extent;
 	setMidpoints(coords);
 	reDraw(coords);
 }
@@ -341,8 +277,17 @@ void shiftFrame(coordSet* coords, double xShift, double yShift){
 	coords->xmin+=xShift*width;
 	coords->ymax+=yShift*height;
 	coords->ymin+=yShift*height;
-	coords->xShift+=xShift;
-	coords->yShift+=yShift;
+
+	if (coords->zoom == 0) {
+		coords->xShift+=xShift;
+		coords->yShift+=yShift;
+	}
+
+	else {
+		coords->xShift+=(xShift/abs(coords->zoom));
+		coords->yShift+=(yShift/abs(coords->zoom));
+	}
+
 	setMidpoints(coords);
 	reDraw(coords);
 }
@@ -358,6 +303,7 @@ void reflect(coordSet* coords){
 	setMidpoints(coords);
 	reDraw(coords);
 }
+
 
 void usage(){
     printf("Usage: benchmark [n] [m] [dim] [max_iter]\n");
@@ -412,7 +358,7 @@ int main( int argc, char *argv[] ){
 		blockSize = atoi(argv[9]);
 	}
 
-
+	#ifndef NOX
 	// Open a new window.
 	if(windowWidth < 2048)
 	gfx_open(windowWidth,windowHeight,"Mandelbrot Fractal");
@@ -425,11 +371,11 @@ int main( int argc, char *argv[] ){
 	// Fill it with a dark blue initially.
 	gfx_clear_color(0,0,255);
 	gfx_clear();
-
+	#endif
 	//draw intial position
 	reDraw(dispCoords);
 
-
+	#ifndef NOX
 	while(1) {
 		// Wait for a key or mouse click.
 		int c = gfx_wait();
@@ -497,6 +443,7 @@ int main( int argc, char *argv[] ){
 		}
 //		} else if(c=='q'){
 	}
+	#endif
 
 	return 0;
 }
